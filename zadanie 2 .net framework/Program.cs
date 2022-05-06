56c7a9c8b79a33dfe3e650a2e8e09a1fb618eff0
michalchowaniec <michal.chowaniec@microsoft.wsei.edu.pl>
2022-05-06 22:11:38 +0200

    Dodaj pliki projektów.

---
+++ zadanie 2 .net framework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_2.net_framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ zadanie 2 .net framework/Shop.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace zadanie_2.net_framework
+{
+    public class Shop
+    {
+        private String name
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private Person[] people
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private Product[] products
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public String Name
+        {
+            get value;
+            set name;
+            
+            
+        }
+
+        public Shop create(string name, Person[] people, Product[] products)
+        {
+            Shop shop = new Shop();
+            shop.name = name;
+            shop.people = people;
+            shop.products = products;
+        }
+
+        public void Print()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
